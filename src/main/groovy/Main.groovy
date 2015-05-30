d7de8562c5a016d4eedf9ed9a73020dfdd223cf6
Masashi Fujita <objectxtreme@gmail.com>
2015-05-31 00:48:33 +0900

    Convert files in parallel
    Powered by GPars.

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -1,135 +1,133 @@
 import ch.qos.logback.classic.Level
 import groovy.transform.CompileStatic
-import groovy.transform.Field
+import groovy.transform.stc.ClosureParams
+import groovy.transform.stc.SimpleType
+import groovy.util.logging.Slf4j
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 import java.nio.file.Path
 import java.nio.file.Paths
-import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 import static java.nio.file.Files.exists
 
-def build_option_parser () {
-    def scriptname = (new File (getClass().protectionDomain.codeSource.location.file)).name
-    def cli = new CliBuilder (usage: "${scriptname} [options] [<repository>...]", stopAtNonOption: false)
-    cli.with {
-        H (longOpt: 'help', "Show this.")
-        v (longOpt: 'verbose', "Be verbose.")
-        N (longOpt: 'dry-run', "Don't modify anything.")
-        A (longOpt: 'expand-all-tabs', "Expand all tabs.")
-        _ (longOpt: 'parallel', "Do parallel conversion", optionalArg: true, args: 1, argName: 'threads')
+@Slf4j
+final class Main {
+    static final Logger rootLogger = LoggerFactory.getLogger Logger.ROOT_LOGGER_NAME
+    static int cntThread = 0
+    static Pattern rxSource = Pattern.compile (/.+\.(?:h|hh|hpp|hxx|h\+\+|c|cc|cpp|cxx|c\+\+|py|pl|java|groovy)$/)
+
+    static void main (String [] args) {
+        CliBuilder cli = build_option_parser ()
+
+        def options = cli.parse args
+
+        if (! options) {
+            System.exit 1
+        }
+
+        if (options.'help') {
+            cli.usage ()
+            System.exit 1
+        }
+
+        if (options.'verbose') {
+            rootLogger.level = Level.INFO
+        }
+
+        if (options.'parallel') {
+            try {
+                cntThread = Integer.parseInt (options.'parallel')
+            }
+            catch (NumberFormatException e) {
+                log.error "Bad # of threads specified: \"{}\"", options.'parallel'
+                System.exit 1
+            }
+        }
+
+        try {
+            def repos = options.arguments ()
+            def scanner = new SourceTextScanner (dryrun: options.'dry-run', expandAllTabs: options.'expand-all-tabs')
+
+            if (! repos) {
+                repos = ['.']
+            }
+            for (String r in repos) {
+                rootLogger.info "Processing repository: {}", r
+                eachRepositoryFiles (Paths.get (r), Main.&isTarget) {
+                    scanner.normalize it
+                }
+            }
+        }
+        catch (Exception e) {
+            rootLogger.error "Something wrong happen! ({})", e.message
+            System.exit 1
+        }
+
+        System.exit 0
     }
-    cli
-}
 
-def cli = build_option_parser ()
-
-def options = cli.parse args
-
-if (! options) {
-    System.exit 1
-}
-
-if (options.'help') {
-    cli.usage ()
-    System.exit 1
-}
-
-@Field Logger rootLogger = LoggerFactory.getLogger Logger.ROOT_LOGGER_NAME
-
-if (options.'verbose') {
-    rootLogger.level = Level.INFO
-}
-
-@Field cntThreads = 0
-
-if (options.'parallel') {
-    cntThreads = Integer.parseInt (options.'parallel')
-}
-
-
-void eachRepositoryFiles (Path repo, Closure closure) {
-    Process files
-
-    if (exists (repo.resolve ('.hg'))) {
-        // log.info ".hg/ found"
-        files = ["hg", "files"].execute ([], repo.toFile ())
-    }
-    else if (exists (repo.resolve ('.git'))) {
-        // log.info ".git/ found"
-        files = ["git", "ls-files"].execute ([], repo.toFile ())
-    }
-    else {
-        throw new IOException ("${repo} is neither git nor mercurial repository.")
+    static final CliBuilder build_option_parser () {
+        def scriptname = (new File (Main.protectionDomain.codeSource.location.file)).name
+        def cli = new CliBuilder (usage: "${scriptname} [options] [<repository>...]", stopAtNonOption: false)
+        cli.with {
+            H (longOpt: 'help', "Show this.")
+            v (longOpt: 'verbose', "Be verbose.")
+            N (longOpt: 'dry-run', "Don't modify anything.")
+            A (longOpt: 'expand-all-tabs', "Expand all tabs.")
+            _ (longOpt: 'parallel', "Do parallel conversion", optionalArg: true, args: 1, argName: 'threads')
+        }
+        cli
     }
 
-    if (0 < cntThreads) {
-        rootLogger.info "Processing with {} threads", cntThreads
-        String [] lines = files.in.readLines ().toArray ()
-        groovyx.gpars.GParsPool.withPool (cntThreads) {
-            lines.eachParallel { String l ->
+    static final void eachRepositoryFiles (Path repo,
+                                           @ClosureParams (value=SimpleType, options=['java.nio.file.Path']) Closure<Boolean> predicate,
+                                           @ClosureParams (value=SimpleType, options=['java.nio.file.Path']) Closure closure) {
+        Process files
+
+        if (exists (repo.resolve ('.hg'))) {
+            // log.info ".hg/ found"
+            files = ["hg", "files"].execute ([], repo.toFile ())
+        }
+        else if (exists (repo.resolve ('.git'))) {
+            // log.info ".git/ found"
+            files = ["git", "ls-files"].execute ([], repo.toFile ())
+        }
+        else {
+            throw new IOException ("${repo} is neither git nor mercurial repository.")
+        }
+
+        if (1 < cntThread) {
+            rootLogger.info "Processing with {} threads", this.cntThread
+            String [] lines = files.in.readLines ().toArray ()
+            groovyx.gpars.GParsPool.withPool (this.cntThread) {
+                lines.eachParallel { String l ->
+                    Path target = repo.resolve l
+                    predicate.delegate = target
+                    if (predicate (target)) {
+                        log.debug "Process: {}", target.toString ()
+                        closure.delegate = target
+                        closure target
+                    }
+                }
+            }
+        }
+        else {
+            files.in.eachLine { String l ->
                 Path target = repo.resolve l
-                closure.delegate = target
-                closure target
+                predicate.delegate = target
+                if (predicate (target)) {
+                    log.debug "Process: {}", target.toString ()
+                    closure.delegate = target
+                    closure target
+                }
             }
         }
     }
-    else {
-        files.in.eachLine { String l ->
-            Path target = repo.resolve l
-            closure.delegate = target
-            closure target
-        }
+
+    @CompileStatic
+    static final boolean isTarget (Path path) {
+        rxSource.matcher (path.fileName.toString ())?.matches ()
     }
 }
-
-@CompileStatic
-void eachRepositoryFiles (String path, Closure closure) {
-    eachRepositoryFiles (Paths.get (path), closure)
-}
-
-@CompileStatic
-void eachRepositoryFiles (Path repo, Closure predicate, Closure closure) {
-    eachRepositoryFiles (repo) { Path path ->
-        if (predicate (path)) {
-            closure.delegate = path
-            closure path
-        }
-    }
-}
-
-@CompileStatic
-void eachRepositoryFiles (String repo, Closure predicate, Closure closure) {
-    eachRepositoryFiles Paths.get (repo), predicate, closure
-}
-
-def scanner = new SourceTextScanner (dryrun: options.'dry-run', expandAllTabs: options.'expand-all-tabs')
-
-@Field Pattern rxSource = Pattern.compile (/.+\.(?:h|hh|hpp|hxx|h\+\+|c|cc|cpp|cxx|c\+\+|py|pl|java|groovy)$/)
-
-@CompileStatic
-boolean isTarget (Path path) {
-    Matcher m = rxSource.matcher path.fileName.toString ()
-    m.matches ()
-}
-
-try {
-    def repos = options.arguments ()
-    if (! repos) {
-        repos = ['.']
-    }
-    for (def r in repos) {
-        rootLogger.info "Processing repository: {}", r
-        eachRepositoryFiles (r, this.&isTarget) { Path p ->
-            scanner.normalize p
-        }
-    }
-}
-catch (Exception e) {
-    rootLogger.error "Something wrong happen! ({})", e.message
-    System.exit 1
-}
-
-System.exit 0
